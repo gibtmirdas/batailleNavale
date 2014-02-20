0fbb1f2f743b0c80c7f980d8cfb84d5f2503b2f8
gibtmirdas <gibtmirdas@hotmail.fr>
2014-02-20 16:55:08 +0100

    Init

---
+++ ServerBNP/src/Server.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+
+public class Server implements Runnable {
+	ServerSocket ss;
+	QueueManager queue;
+	Map<String, Connection> mapping;
+
+	public Server(int port) {
+		queue = new QueueManager(mapping);
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Socket s = ss.accept();
+				Connection connect = new Connection(s, mapping);
+				new Thread(connect).start();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
