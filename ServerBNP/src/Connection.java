0cf7d3ddafdb20ef718d10593e9ea81240a66ba8
gibtmirdas <gibtmirdas@hotmail.fr>
2014-02-27 16:53:17 +0100

    merge1

--- ServerBNP/src/Connection.java
+++ ServerBNP/src/Connection.java
@@ -1,23 +1,31 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import packet.Packet;
+import packet.PacketBuilder;
+import packet.PacketHello;
 
 public class Connection implements Runnable {
 	Socket s;
 	OutputStream os;
 	BufferedReader br;
 	Game g;
+	Server serv;
 	Map<String, Connection> mapping;
 	String id;
 
-	public Connection(Socket s, Map<String, Connection> mapping) {
+	public Connection(Socket s, Server srv) {
 		this.s = s;
-		this.mapping = mapping;
+		this.serv = srv;
+		this.mapping = serv.mapping;
 		try {
 			os = s.getOutputStream();
 			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
@@ -31,11 +39,15 @@ public class Connection implements Runnable {
 	public void run() {
 		Packet p = null;
 		try {
-			p = new Packet(br.readLine().getBytes());
+			p = PacketBuilder.build(br.readLine().getBytes());
+			PacketHello ph = (PacketHello) (p);
+			mapping.put(Arrays.toString(ph.getMacAddress()), this);
+			serv.queue.notifyQueue(Arrays.toString(ph.getMacAddress()));
 			// TODO get mac
 			// mapping.put(p.mac, this);
+
 			while (true) {
-				p = new Packet(br.readLine().getBytes());
+				p = PacketBuilder.build(br.readLine().getBytes());
 				if (g != null) {
 					g.traiterPacket(p);
 				}
