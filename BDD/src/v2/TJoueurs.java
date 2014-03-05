0819a60fa82eeae7343c4ba0e175d4984773f032
gibtmirdas <gibtmirdas@hotmail.fr>
2014-03-05 11:30:17 +0100

    Set DB v2

---
+++ BDD/src/v2/TJoueurs.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package v2;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 
+ * @author antho
+ */
+public class TJoueurs extends AbstractTable {
+
+	private final String tableName = "joueurs";
+
+	public TJoueurs() {
+		linkToTable = Storage.getInstance().getCollection(tableName);
+	}
+
+	/**
+	 * 
+	 * @param args
+	 *            args[0]:Pseudo ...
+	 * @return
+	 */
+	@Override
+	public void insert(List<Object> args) {
+		TCartes tcartes = new TCartes();
+		Integer cardID = 1, defaultScore = 0, defaultCredit = 0;
+		String defaultPassword = "";
+		BasicDBObject insertQuery = new BasicDBObject();
+		List<Integer> cardList = new ArrayList<>();
+		cardList.add(cardID);
+		cardList.add(cardID);
+
+		insertQuery.put("id", this.getLastID() + 1);
+		insertQuery.put("pseudo", args.get(0));
+		insertQuery.put("cardID", cardList);
+		insertQuery.put("score", defaultScore);
+		insertQuery.put("password", defaultPassword);
+		insertQuery.put("credit", defaultCredit);
+		linkToTable.insert(insertQuery);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Integer> getCardsById(int id) {
+
+		BasicDBObject searchQuery = new BasicDBObject();
+		BasicDBObject fields = new BasicDBObject().append("cardID", 1);
+		searchQuery.put("id", id);
+		try {
+			return (List<Integer>) linkToTable.find(searchQuery, fields).next()
+					.get("cardID");
+		} catch (RuntimeException re) {
+			System.err.println("Player " + id + " has no card /!\\");
+			return null;
+		}
+
+	}
+
+	public void addCardToPlayer(String cardName, int playerID) {
+		TCartes cartes = new TCartes();
+		int cardID = cartes.getIdByCriteria("nom", cardName);
+		System.err.print(cardID);
+		List<Integer> cardList = getCardsById(playerID);
+		cardList.add(cardID);
+		HashMap<String, Object> updateList = new HashMap<>();
+		updateList.put("cardID", cardList);
+		updateById(playerID, updateList);
+	}
+}
