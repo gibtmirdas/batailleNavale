cff441efd9a436ae9c8ea5f4fe8b0cb02668a0b5
Mabite <belli.ol@bluewin.ch>
2014-03-12 10:25:25 +0100

    J'ai recouvert jayro de ma semance!

--- ServerBNP/src/packet/PacketBuilder.java
+++ ServerBNP/src/packet/PacketBuilder.java
@@ -1,5 +1,8 @@
 package packet;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class PacketBuilder {
 
 	public static Class<?> classes[] = { packet.PacketHello.class,
@@ -25,5 +28,17 @@ public class PacketBuilder {
 				return i;
 		}
 		return -1;
+	}
+	public static byte[] readPacket(InputStream is) throws IOException{
+		byte opcodeID, size;
+		opcodeID = (byte)is.read();
+		size = (byte)is.read();
+		byte[] datas = new byte[size+2];
+		datas[0] = opcodeID;
+		datas[1] = size;
+		for (int i = 2; i < datas.length; i++) {
+			datas[i] = (byte) is.read();
+		}
+		return datas;
 	}
 }
