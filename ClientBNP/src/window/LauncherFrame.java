bdeaad86e8d09e8c955452e48b0a75a7a2b51609
anthony <anthony.boulmier.cfpt@gmail.com>
2014-03-19 11:49:10 +0100

    gui manager

--- ClientBNP/src/window/LauncherFrame.java
+++ ClientBNP/src/window/LauncherFrame.java
@@ -4,26 +4,33 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
-
+import javax.swing.JPanel;
 
 public class LauncherFrame extends JFrame {
-	private static final long serialVersionUID = 1L;
-	int WIDTH = 600, HEIGHT = 500;
-	public LauncherFrame(){
-		this.setTitle("Launcher v0.1");
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
-    	//Add content to the window.
-    	this.add(new LauncherTabs(WIDTH,HEIGHT), BorderLayout.CENTER);
-    	//Display the window.
-    	this.pack();
-    	this.setVisible(true);
-    	
-    	
+
+    private static final long serialVersionUID = 1L;
+    int WIDTH = 600, HEIGHT = 500;
+
+    public LauncherFrame() {
+        this.setTitle("Launcher v0.1");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        //Add content to the window.
+        this.add(new LoginFrame(null));
+        //Display the window.
+        this.pack();
+        this.setVisible(true);
     }
- 
+
+    public void cleanView() {
+        this.remove(0);
+    }
+
+    public void setView(JPanel panel) {
+        this.add(panel);
+    }
+
     public static void main(String[] args) {
-    	LauncherFrame frame = new LauncherFrame();
-    	
+        LauncherFrame frame = new LauncherFrame();
     }
 }
