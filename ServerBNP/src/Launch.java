7bba4d5040d0a60b0ba47f6529ef8e566301b7a1
root <jayrorenaldazcor@gmail.com>
2014-03-26 10:09:31 +0100

    1version

--- ServerBNP/src/Launch.java
+++ ServerBNP/src/Launch.java
@@ -4,8 +4,10 @@ import db.TJoueurs;
 public class Launch {
 
 	public static void main(String[] args) {
-
-		new Thread(new Server(12345)).start();
+			
+//		new Thread(new Server(12345)).start();
+//		System.out.println("Server is launched!");
+		new Thread(new Server(Integer.parseInt(args[0]))).start();
 		System.out.println("Server is launched!");
 	}
 }
