c791d32fbc1e0e846870a4f19c8bc61f9695481c
Mabite <belli.olivier@gmail.com>
2014-03-26 12:00:07 +0100

    J'ai éjaculé

--- ClientBNP/src/window/LauncherTabShopView.java
+++ ClientBNP/src/window/LauncherTabShopView.java
@@ -1,6 +1,9 @@
 package window;
 
-import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JPanel;
 
@@ -14,10 +17,27 @@ public class LauncherTabShopView extends JPanel implements PanelNotifiable{
 	
 	public LauncherTabShopView(ClientConnection conn){
 		c = conn;
-		setLayout(new BorderLayout());
-		LauncherShopCardList panel1 = new LauncherShopCardList(conn);
-		add(panel1,BorderLayout.CENTER);
+		setLayout(new GridBagLayout());
+		setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.insets = new Insets(0,0,0,10);
+		c.gridwidth = 1;
+		c.gridheight=1;
+		c.weightx = 0.7;
+		c.weighty=1;
+		c.gridx = 0;
+		c.gridy = 0;
+		Description d = new Description();
+		LauncherShopCardList panel1 = new LauncherShopCardList(conn,d);
 		panel1.init();
+		c.ipady = 10000;
+		add(panel1,c);   
+		c.weightx = 0.3;
+		c.gridx = 1;
+		c.gridy = 0;
+		
+		add(d,c);
 	}
 	
 	@Override
