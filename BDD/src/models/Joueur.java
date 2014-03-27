d840f2b681ac63c11ed701a7029c03ae2fcb100c
anthony <anthony.boulmier.cfpt@gmail.com>
2014-03-27 14:22:46 +0100

    assertion add

--- BDD/src/models/Joueur.java
+++ BDD/src/models/Joueur.java
@@ -10,155 +10,158 @@ import db.TJoueurs;
 
 /**
  * Player object for database
- * 
+ *
  * @author JizzTeam
- * 
+ *
  */
 public class Joueur {
 
-	private String pseudo, password;
-	private int score, credit, id;
-	private ArrayList<Integer> cardsId;
+    private String pseudo, password;
+    private int score, credit, id;
+    private ArrayList<Integer> cardsId;
 
-	public Joueur(String pseudo, String password, ArrayList<Integer> cardsId,
-			int score, int credit, int id) {
-		super();
-		this.id = id;
-		this.pseudo = pseudo;
-		this.password = password;
-		this.cardsId = cardsId;
-		this.score = score;
-		this.credit = credit;
-	}
+    public Joueur(String pseudo, String password, ArrayList<Integer> cardsId,
+            int score, int credit, int id) {
+        assert !pseudo.equals("") && !password.equals("");
+        this.id = id;
+        this.pseudo = pseudo;
+        this.password = password;
+        this.cardsId = cardsId;
+        this.score = score;
+        this.credit = credit;
+    }
 
-	@SuppressWarnings("unchecked")
-	public Joueur(DBObject o) {
-		id = (int) o.get("id");
-		pseudo = (String) o.get("pseudo");
-		password = (String) o.get("password");
-		cardsId = (ArrayList<Integer>) o.get("cardID");
-		score = (int) o.get("score");
-		credit = (int) o.get("credit");
-	}
+    @SuppressWarnings("unchecked")
+    public Joueur(DBObject o) {
+        assert o != null;
+        id = (int) o.get(TJoueurs.ID_FIELD);
+        pseudo = (String) o.get(TJoueurs.NAME_FIELD);
+        password = (String) o.get(TJoueurs.PASSWORD_FIELD);
+        cardsId = (ArrayList<Integer>) o.get(TJoueurs.CARDS_FIELD);
+        score = (int) o.get(TJoueurs.SCORE_FIELD);
+        credit = (int) o.get(TJoueurs.CREDIT_FIELD);
+    }
 
-	public boolean canBuyCard(Carte c) {
-		return this.getCredit() >= c.getCost();
-	}
+    public boolean canBuyCard(Carte c) {
+        return this.getCredit() >= c.getCost();
+    }
 
-	public static boolean canLogin(String username, String password) {
-		TJoueurs tj = new TJoueurs();
-		DBObject o = tj.getById(tj.getIdByCriteria(TJoueurs.NAME_FIELD,
-				username));
-		if (o == null)
-			return false;
-		return o.get(TJoueurs.PASSWORD_FIELD).equals(password);
-	}
+    public static boolean canLogin(String username, String password) {
+        assert !username.equals("") && !password.equals("");
+        TJoueurs tj = new TJoueurs();
+        DBObject o = tj.getById(tj.getIdByCriteria(TJoueurs.NAME_FIELD,
+                username));
+        if (o == null) {
+            return false;
+        }
+        return o.get(TJoueurs.PASSWORD_FIELD).equals(password);
+    }
 
-	/**
-	 * Return a player based on the username, if password is correct. Otherwise,
-	 * return null
-	 * 
-	 * @param username
-	 * @param password
-	 * @return
-	 */
+    /**
+     * Return a player based on the username, if password is correct. Otherwise,
+     * return null
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    public static Joueur getJoueur(String username, String password) {
+        TJoueurs tj = new TJoueurs();
 
-	public static Joueur getJoueur(String username, String password) {
-		TJoueurs tj = new TJoueurs();
+        if (tj.existJoueur(username)) {
+            if (canLogin(username, password)) {
+                Joueur j = new Joueur(tj.getById(tj.getIdByCriteria(
+                        TJoueurs.NAME_FIELD, username)));
+                return j;
+            }
+        }
 
-		if (tj.existJoueur(username))
-			if (canLogin(username, password)) {
-				Joueur j = new Joueur(tj.getById(tj.getIdByCriteria(
-						TJoueurs.NAME_FIELD, username)));
-				return j;
-			}
+        return null;
+    }
 
-		return null;
-	}
+    public boolean buyCard(Carte c) {
+        if (canBuyCard(c)) {
+            setCredit(this.getCredit() - c.getCost());
+            addCard(c);
+        }
+        return false;
+    }
 
-	public boolean buyCard(Carte c) {
-		if (canBuyCard(c)) {
-			setCredit(this.getCredit() - c.getCost());
-			addCard(c);
-		}
-		return false;
-	}
+    private void addCard(Carte c) {
+        TJoueurs tj = new TJoueurs();
+        tj.addCardToPlayer(c.getName(), this.id);
+    }
 
-	private void addCard(Carte c) {
-		TJoueurs tj = new TJoueurs();
-		tj.addCardToPlayer(c.getName(), this.id);
-	}
+    public String getPseudo() {
+        return pseudo;
+    }
 
-	public String getPseudo() {
-		return pseudo;
-	}
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+        TJoueurs tj = new TJoueurs();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TJoueurs.NAME_FIELD, pseudo);
+        tj.updateById(this.id, map);
+    }
 
-	public void setPseudo(String pseudo) {
-		this.pseudo = pseudo;
-		TJoueurs tj = new TJoueurs();
-		HashMap<String, Object> map = new HashMap<>();
-		map.put(TJoueurs.NAME_FIELD, pseudo);
-		tj.updateById(this.id, map);
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+        TJoueurs tj = new TJoueurs();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TJoueurs.PASSWORD_FIELD, password);
+        tj.updateById(this.id, map);
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-		TJoueurs tj = new TJoueurs();
-		HashMap<String, Object> map = new HashMap<>();
-		map.put(TJoueurs.PASSWORD_FIELD, password);
-		tj.updateById(this.id, map);
-	}
+    public ArrayList<Integer> getCardId() {
+        return cardsId;
+    }
 
-	public ArrayList<Integer> getCardId() {
-		return cardsId;
-	}
+    public void setCardId(ArrayList<Integer> cardId) {
+        this.cardsId = cardId;
+        TJoueurs tj = new TJoueurs();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TJoueurs.CARDS_FIELD, cardId);
+        tj.updateById(this.id, map);
+    }
 
-	public void setCardId(ArrayList<Integer> cardId) {
-		this.cardsId = cardId;
-		TJoueurs tj = new TJoueurs();
-		HashMap<String, Object> map = new HashMap<>();
-		map.put(TJoueurs.CARDS_FIELD, cardId);
-		tj.updateById(this.id, map);
-	}
+    public int getScore() {
+        return score;
+    }
 
-	public int getScore() {
-		return score;
-	}
+    public void setScore(int score) {
+        this.score = score;
+        TJoueurs tj = new TJoueurs();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TJoueurs.SCORE_FIELD, score);
+        tj.updateById(this.id, map);
+    }
 
-	public void setScore(int score) {
-		this.score = score;
-		TJoueurs tj = new TJoueurs();
-		HashMap<String, Object> map = new HashMap<>();
-		map.put(TJoueurs.SCORE_FIELD, score);
-		tj.updateById(this.id, map);
-	}
+    public int getCredit() {
+        return credit;
+    }
 
-	public int getCredit() {
-		return credit;
-	}
+    public void setCredit(int credit) {
+        this.credit = credit;
+        TJoueurs tj = new TJoueurs();
+        HashMap<String, Object> map = new HashMap<>();
+        map.put(TJoueurs.CREDIT_FIELD, credit);
+        tj.updateById(this.id, map);
+    }
 
-	public void setCredit(int credit) {
-		this.credit = credit;
-		TJoueurs tj = new TJoueurs();
-		HashMap<String, Object> map = new HashMap<>();
-		map.put(TJoueurs.CREDIT_FIELD, credit);
-		tj.updateById(this.id, map);
-	}
+    public int getId() {
+        return id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Deck getDeck() {
-		TDecks td = new TDecks();
-		return new Deck(td.getIdByCriteria(TDecks.OWNER_FIELD, this.id), true);
-	}
+    public Deck getDeck() {
+        TDecks td = new TDecks();
+        return new Deck(td.getIdByCriteria(TDecks.OWNER_FIELD, this.id), true);
+    }
 }
