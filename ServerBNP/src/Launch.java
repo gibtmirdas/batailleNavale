5b5fd97d273c1ec185d272821daf087b189a5e39
gibtmirdas <gibtmirdas@hotmail.fr>
2014-04-03 15:31:40 +0200

    MERGE !

--- ServerBNP/src/Launch.java
+++ ServerBNP/src/Launch.java
@@ -1,12 +1,8 @@
-
 import db.TJoueurs;
 
 public class Launch {
 
 	public static void main(String[] args) {
-			
-//		new Thread(new Server(12345)).start();
-//		System.out.println("Server is launched!");
 		new Thread(new Server(Integer.parseInt(args[0]))).start();
 		System.out.println("Server is launched!");
 	}
