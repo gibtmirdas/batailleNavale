a3e79c5853702ecb748a3bc096f17023de3daa4f
gibtmirdas <gibtmirdas@hotmail.fr>
2014-04-03 15:01:21 +0200

    a

--- ClientBNP/src/Launcher.java
+++ ClientBNP/src/Launcher.java
@@ -2,18 +2,16 @@ import lib.Constante;
 import GUIManager.GUIManager;
 import connection.ClientConnection;
 
-
-
-public class Launcher{
+public class Launcher {
 
 	public static void main(String[] args) {
-            
-			Constante.PORT = Integer.parseInt(args[0]);
-			Constante.ADDRESS  = args[1];
-//			Constante.PORT = Integer.parseInt("12345");
-//			Constante.ADDRESS  = "129.194.184.98";
-			GUIManager gui = GUIManager.getInstance();
-            ClientConnection conn = new ClientConnection();	
-            
+
+		// Constante.PORT = Integer.parseInt(args[0]);
+		// Constante.ADDRESS = args[1];
+		Constante.PORT = Integer.parseInt("12345");
+		Constante.ADDRESS = "129.194.184.98";
+		GUIManager gui = GUIManager.getInstance();
+		ClientConnection conn = new ClientConnection();
+
 	}
 }
